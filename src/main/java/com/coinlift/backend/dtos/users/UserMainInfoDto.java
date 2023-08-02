1ddbae3b921b79ffe05d686c730ad0215be9527b
Vladyslav <vladsw764@icloud.com>
2023-08-02 11:28:25 +0300

    remove field 'userId'

--- src/main/java/com/coinlift/backend/dtos/users/UserMainInfoDto.java
+++ src/main/java/com/coinlift/backend/dtos/users/UserMainInfoDto.java
@@ -2,13 +2,7 @@ package com.coinlift.backend.dtos.users;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-import java.util.UUID;
-
 public record UserMainInfoDto(
-
-        @Schema(description = "The unique identifier of the user.", example = "123e4567-e89b-12d3-a456-426614174000")
-        UUID userId,
-
         @Schema(description = "The username of the user.", example = "john_doe")
         String username,
 
